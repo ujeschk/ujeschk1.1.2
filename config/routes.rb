e32a47e2d85e3405c9e6dc59b54d928afdb680d5
ujeschk <31452890+ujeschk@users.noreply.github.com>
2018-01-25 15:28:48 +0300

    Test added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'ahmet2/index' => 'ahmet2#index'
+  get 'ahmet2' => 'ahmet2#index'
+  get 'test1' => 'test1#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
